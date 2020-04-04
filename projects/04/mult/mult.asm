35f141fb10b90742cbd2d56273c2a4c6476710ac
lesteven <stevenle2011@gmail.com>
2020-04-04 14:29:03 -0700

    refactor mult again

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,34 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-    @R0
-    D=M
-    @inc
-    M=D // set RAM[inc] to R[0]
 
 // reset R[2] to zero
-    @0
-    D=0
     @R2
-    M=D
+    M=0
 
-    @END // if R[1] is zero, set R[2] to 0, skip MULT and go to END
+    @R0
+    D=M
+    @END // if R[0] is zero, skip MULT and go to END
+    D;JEQ
+
     @R1
     D=M
+    @END // if R[1] is zero, skip MULT and go to END
     D;JEQ
 
 // multiplication loop
   (MULT)
-    @inc
+    @R0
     D=M // grab increment value
     @R2
-    D=D+M // add increment value to R[2] and store in D Reg
-    M=D // set D reg value to R[2]
+    M=D+M // increment R[2] by value of R[0]
     
     @R1
-    D=M 
-    D=D-1 // get RAM[1] value, store in D Reg and decrease by 1
-    M=D // set RAM[1] to D reg value
+    M=M-1 // decrement R[1] by 1
+    D=M
 
     @MULT // loop again if value is greater than 0
     D;JGT 
