181f3efbdeeaa4baf8b159adfc4ad096fd4c1347
lesteven <stevenle2011@gmail.com>
2020-04-22 19:55:28 -0700

    add assembly parser

--- projects/06/assembler/assembler.py
+++ projects/06/assembler/assembler.py
@@ -1,21 +1,41 @@
 import sys
 
 
-def parseLine(line):
+def parseLine(line, num):
     strippedLine = line.lstrip().rstrip()
     if len(strippedLine) == 0 or strippedLine[0] == "" \
-        or strippedLine[0] == "/":
+        or strippedLine[0] == "/" or strippedLine[0] == "(":
         return None
-    return strippedLine
+
+    # remove comments after code
+    modLine = strippedLine.partition('//')[0].rstrip()
+
+    instructionType = 'a-instruction' if modLine[0] == '@' \
+        else 'c-instruction'
+
+    parsed = { 
+        'line': modLine, 
+        'address': num,
+        'instructionType': instructionType
+    }
+    if instructionType != 'a-instruction':
+        splitColon = modLine.split(';')
+        parsed['jump'] = splitColon[1] if len(splitColon) > 1 else ''
+
+        splitEqual = splitColon[0].split('=')
+        parsed['comp'] = splitEqual[1] if len(splitEqual) > 1 else ''
+        parsed['dest'] = splitEqual[0]
+
+    return parsed
 
 def assembler(fileName):
     with open(fileName,"r") as f:
         num = 0
         for line in f:
-            parsed = parseLine(line)
-            if (parsed == None):
+            parsed = parseLine(line, num)
+            if parsed == None:
                 continue
-            print(str(num) + " " + parsed)
+            print(parsed)
             num += 1
 
 
